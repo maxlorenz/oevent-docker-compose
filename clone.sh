e3819162b0d4a73f8528c218ea1ebb7324ce3015
Max Lorenz <max-lorenz@gmx.net>
2018-05-21 11:15:24 +0200

    Add clone shell script

---
+++ clone.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/fossasia/open-event-legacy v1
+
+git clone https://github.com/fossasia/open-event-server v2-server
+git clone https://github.com/fossasia/open-event-frontend v2-frontend
