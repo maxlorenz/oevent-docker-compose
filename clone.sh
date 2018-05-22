6b32c0c8757487eacf2b7ad455d96625b3085ff4
Max Lorenz <ml@cypp.hamburg>
2018-05-22 14:44:06 +0200

    fix windows line endings

--- clone.sh
+++ clone.sh
@@ -1,4 +1,5 @@
-git clone https://github.com/fossasia/open-event-legacy v1
+#!/usr/bin/env sh
 
+git clone https://github.com/fossasia/open-event-legacy v1
 git clone https://github.com/fossasia/open-event-server v2-server
 git clone https://github.com/fossasia/open-event-frontend v2-frontend
