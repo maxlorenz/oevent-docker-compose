b5ee10ddb45dc3c6007f4adf7cd13a815db14df2
Kreijstal <2415206+Kreijstal@users.noreply.github.com>
2018-05-24 11:02:14 +0200

    Update clone.sh
    double --depth=1

--- clone.sh
+++ clone.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 
 git clone --depth=1 https://github.com/fossasia/open-event-legacy v1
-git clone --depth=1 https://github.com/fossasia/open-event-server v2-server --depth=1
+git clone --depth=1 https://github.com/fossasia/open-event-server v2-server
 git clone --depth=1 https://github.com/fossasia/open-event-frontend v2-frontend
